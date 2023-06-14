8dbddc21299540468897d3a2422bc68ad3742487
Jon Lanz <Jon.Lanz@dreamworks.com>
2023-06-14 13:25:54 -0700

    MOONRAY-4952 Add CMake options to set CXX11 ABI and abi-version (#41)
    * Add option to set _GLIDCXX_USE_CXX11_ABI
    
    * Don't define _GLIBCXX_USE_CXX11_ABI
    
    * Remove duplicate cmake modules/files

--- moonray/moonray_arras/cmake/MoonrayArrasCompileFeatures.cmake
+++
@@ -1,8 +0,0 @@
-
-function(MoonrayArras_cxx_compile_features target)
-    target_compile_features(${target}
-        PRIVATE
-            cxx_std_17
-    )
-endfunction()
-
--- moonray/moonray_arras/cmake/MoonrayArrasLinkOptions.cmake
+++
@@ -1,9 +0,0 @@
-
-function(MoonrayArras_link_options target)
-    target_link_options(${target}
-        PRIVATE
-            -Wl,--enable-new-dtags              # Use RUNPATH instead of RPATH
-    )
-endfunction()
-
-
