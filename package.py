687ab53917890957689d723579657df0c5b50ad5
Mahovsky, Jeff (NBCUniversal) <jeff.mahovsky@dreamworks.com>
2024-12-13 13:49:33 -0800

    Update package.py OMR version. (#121)

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '1.6'
+    _version = '1.7'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
