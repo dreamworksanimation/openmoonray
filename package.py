04c63bc269d25d9523a9df4e5552dc703a38be3a
206519211_nbcuni <randy.packer@dreamworks.com>
2025-07-15 14:05:44 -0700

    [skip ci]Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '2.18'
+    _version = '2.19'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
