c8db65cbd6bd4ff3dd42082392cdcda8f9a52b45
Woods, Ron (NBCUniversal) <Ron.Woods@dreamworks.com>
2025-09-22 21:18:33 -0700

    Rwoods/cm 18876 (#182)
    * CM-18876: re-add changes on a clean openmoonray base. Cleaned up the variants, re-ordered, removed CentOS 7 variants added .openmoonray_variant-# for github action building and testing.  Fixed up tests so only the one variant is pulled in by making the correct test dictionary entries. Separated out the moonshine_dwa requirements so that they are only added if moonshine_dwa repo is present. Upgraded to ispc-1.26 and python version selected be refplat. Enabled building of moonray_tools if repo is present.
    
    * CM-18876: update submodules.
    
    * Sync submodules for openmoonray.
    
    * Sync submodules for openmoonray.
    
    * CM-18876: re-add changes on a clean openmoonray base. Cleaned up the variants, re-ordered, removed CentOS 7 variants added .openmoonray_variant-# for github action building and testing.  Fixed up tests so only the one variant is pulled in by making the correct test dictionary entries. Separated out the moonshine_dwa requirements so that they are only added if moonshine_dwa repo is present. Upgraded to ispc-1.26 and python version selected be refplat. Enabled building of moonray_tools if repo is present.
    
    * submodule update.
    
    * submodule update.
    
    * Sync submodules for openmoonray.
    
    * Sync submodules for openmoonray.
    
    * Sync submodules for openmoonray.
    
    * CM-18876: moved .yml workflows from rats_canonical_tin.
    
    * CM-18876: moved to openmoonray.
    
    * Sync submodules for openmoonray.
    
    * CM-18876: moved to openmoonray.
    
    * CM-18876: Removed hard coding to test branch but may need to put it back for further testing.
    
    * Update .github/workflows/SubmoduleUpdate.yml
    
    Co-authored-by: Lanz, Jon (NBCUniversal) <jon.lanz@dreamworks.com>
    
    * CM-18876: revert back to ispc-1.20 as ispc-1.26 is not ready to go.
    
    ---------
    
    Co-authored-by: 206817371_nbcuni <206817371@tfayd.com>
    Co-authored-by: Lanz, Jon (NBCUniversal) <jon.lanz@dreamworks.com>

---
+++ .github/workflows/scripts/build_and_test_variant.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+# Expects to be run from the github.workspace directory with one parameter which is the
+# variant to build and run unit tests on.
+# build_and_test_variant.sh <variant1> <variant2> ... <variantn>
+
+if [ $# -lt 1 ]
+then
+    printf "Usage:\n\t$0 variant#+\n"
+    exit 1
+fi
+
+# Set the variants to build and test.
+variants=$*
+echo "Building and tests variant(s) " ${variants}
+
+# define environment variables that are setup by the studio.
+export REL_BOOT_DIR=/rel/boot
+export OS_RELEASE=$(/bin/cat /etc/redhat-release | sed -r 's/.*release ([0-9]+).*/ws\1/')
+export RATS_CANONICAL_DIR=$(pwd)
+
+# Setup access to rez.
+source /rel/boot/rez/rezx.sh
+export REZ_LOCAL_PACKAGES_PATH=$(pwd)/installs
+export REZ_PACKAGES_PATH=${REZ_PRE_PACKAGES_PATH}:${REZ_LOCAL_PACKAGES_PATH}:${REZ_RELEASE_PACKAGES_PATH}:/rel/rez/third_party:/rel/rez/pypi:/rel/lang/python/packages:${REZ_POST_PACKAGES_PATH}
+
+# Change to the openmoonray source directory
+cd source/openmoonray
+
+# Build the variants.
+rez-env buildtools -c "rez-build -i --variants ${variants}"
+
+# Run unit tests on each variant.
+for variant in ${variants}
+do
+    rez-env buildtools -c "rez-test openmoonray variant${variant} || rez-test openmoonray retry_variant${variant}"
+    if [ ${variant} -eq 0 ]
+    then
+        rez-env buildtools -c "rez-test openmoonray rats || rez-test openmoonray retry_rats"
+    # elif [ ${variant} -eq 1 ]
+    # then
+    #     rez-env buildtools -c "rez-test openmoonray rats-debug || rez-test openmoonray retry_rats-debug"
+    fi
+done
--- package.py
+++ package.py
@@ -29,38 +29,96 @@ help = ('For assistance, '
         "please contact the folio's owner at: moonbase-dev@dreamworks.com")
 
 variants = [
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'usd_core-0.22.5.x', 'openimageio-2.3.20.0.x', 'gcc-11.x',       'openvdb-11', 'zlib-1.2.11.x'],
-    ['os-rocky-9', 'opt_level-debug',    'refplat-vfx2023.1', 'usd_core-0.22.5.x', 'openimageio-2.3.20.0.x', 'gcc-11.x',       'openvdb-11', 'zlib-1.2.11.x'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'usd_core-0.22.5.x', 'openimageio-2.3.20.0.x', 'clang-17.0.6.x', 'openvdb-10', 'zlib-1.2.11.x'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'usd_core-0.23.8.x', 'openimageio-2.4.8.0.x',  'gcc-11.x',       'openvdb-11', 'zlib-1.2.11.x'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'usd_core-0.24.3.x', 'openimageio-2.4.8.0.x',  'gcc-11.x',       'openvdb-11', 'zlib-1.2.11.x'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2022.0', 'usd_core-0.22.5.x', 'openimageio-2.3.20.0.x', 'gcc-9.3.x.1',    'openvdb-9',  'zlib-1.2.11.x', 'opensubdiv-3.5.0.x.0'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2024.0', 'usd_core-0.24.3.x', 'openimageio-2.4.8.0.x',  'gcc-11.x',       'openvdb-11', 'zlib-1.2.11.x'],
-
-    ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2022.0', 'usd_core-0.22.5.x', 'openimageio-2.3.20.0.x', 'gcc-9.3.x.1', 'openvdb-9', 'zlib-1.2.8.x.2', 'opensubdiv-3.5.0.x.0'],
-    ['os-CentOS-7', 'opt_level-debug',    'refplat-vfx2022.0', 'usd_core-0.22.5.x', 'openimageio-2.3.20.0.x', 'gcc-9.3.x.1', 'openvdb-9', 'zlib-1.2.8.x.2', 'opensubdiv-3.5.0.x.0'],
+    [
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2023.1',
+        'usd_core-0.23.8.x',
+        'openimageio-2.4.8.0.x',
+        'gcc-11.x',
+        'openvdb-11',
+        'zlib-1.2.11.x'
+    ],
+    [
+        'os-rocky-9',
+        'opt_level-debug',
+        'refplat-vfx2023.1',
+        'usd_core-0.23.8.x',
+        'openimageio-2.4.8.0.x',
+        'gcc-11.x',
+        'openvdb-11',
+        'zlib-1.2.11.x'
+    ],
+    [
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2023.1',
+        'usd_core-0.24.3.x',
+        'openimageio-2.4.8.0.x',
+        'gcc-11.x',
+        'openvdb-11',
+        'zlib-1.2.11.x'
+    ],
+    [
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2023.1',
+        'usd_core-0.22.5.x',
+        'openimageio-2.3.20.0.x',
+        'gcc-11.x',
+        'openvdb-11',
+        'zlib-1.2.11.x'
+    ],
+    [
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2023.1',
+        'usd_core-0.22.5.x',
+        'openimageio-2.3.20.0.x',
+        'clang-17.0.6.x',
+        'openvdb-10',
+        'zlib-1.2.11.x'
+    ],
+    [
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2024.0',
+        'usd_core-0.24.3.x',
+        'openimageio-2.4.8.0.x',
+        'gcc-11.x',
+        'openvdb-11',
+        'zlib-1.2.11.x'
+    ],
+    [
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2022.0',
+        'usd_core-0.22.5.x',
+        'openimageio-2.3.20.0.x',
+        'gcc-9.3.x.1',
+        'openvdb-9',
+        'zlib-1.2.11.x',
+        'opensubdiv-3.5.0.x.0'
+    ],
 ]
 
 conf_rats_variants = variants[0:2]
-conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
+
+# Add ephemeral package to each variant.
+for i in range(len(variants)):
+    variants[i].insert(0, '.openmoonray_variant-%d' % i)
 
 requires = [
-    'alembic_utilities-10',
-    'amorphous',
     'boost',
-    'cfx_fur-10',
     'cuda-12.1.0.x',
     'curl_no_ldap-7.49.1.x',
     'embree-4.2.0.x',
-    'geometry',
-    'gtypes',
     'imath-3',
     'jsoncpp-1.9.5',
     'libmicrohttpd-0.9.71.x.1',
     'log4cplus-1.1.2.x',
     'lua',
     'mkl',
-    'oceanlib-10',
     'openexr',
     'openimagedenoise',
     'opensubdiv',
@@ -71,7 +129,16 @@ requires = [
     'tbb',
     'usd_imaging',
     'uuid-1.0.0',
-    'willow-10.2.0.x.4.32',
+]
+
+dwa_extras = [
+    'amorphous',
+    'alembic_utilities-10',
+    'cfx_fur-10',
+    'geometry',
+    'gtypes',
+    'oceanlib-10',
+    'willow-10.2.0.x.4.34',
 ]
 
 private_build_requires = [
@@ -79,23 +146,91 @@ private_build_requires = [
     'cmake',
     'cppunit',
     'ispc-1.20.0.x',
-    'python-3.9|3.10|3.11',
+    'python',
 ]
+
+# If moonshine_dwa is available add the extra rez packages that it needs into the requires.
+if os.path.exists('moonray/moonshine_dwa/CMakeLists.txt'):
+    requires.extend(dwa_extras)
 
 # Create dictionary of tests for the rez-test command
 # First, add unit tests entry for each variant
-commandstr = lambda i: "cd build/"+os.path.join(*variants[i])+"; ctest --no-tests=error -j $(nproc) -L 'unit'"
+commandstr = lambda i: "cd build/"+os.path.join(*variants[i])+"; ctest --no-tests=error -j $(nproc) -L 'unit' --output-on-failure"
 testentry = lambda i: ("variant%d" % i,
                        { "command": commandstr(i),
-                         "requires": ["cmake"] + variants[i] } )
+                         "requires": ["cmake"],
+                         "on_variants": {
+                             "type": "requires",
+                             "value": [".openmoonray_variant-%d" % i]
+                         },
+                         "run_on": "explicit"
+                     } )
 testlist = [testentry(i) for i in range(len(variants))]
+
+# Second, add a retry unit tests entry for each variant
+retrycommandstr = lambda i: "cd build/"+os.path.join(*variants[i])+"; ctest --no-tests=error -j $(nproc) -L 'unit' --rerun-failed --output-on-failure"
+retrytestentry = lambda i: ("retry_variant%d" % i,
+                            { "command": retrycommandstr(i),
+                              "requires": ["cmake"],
+                              "on_variants": {
+                                  "type": "requires",
+                                  "value": [".openmoonray_variant-%d" % i]
+                              },
+                              "run_on": "explicit"
+                          } )
+testlist.extend([retrytestentry(i) for i in range(len(variants))])
 
 # Next, add rats tests entry
 rats_variant = 0
-rats_cmd = "cd build/"+os.path.join(*variants[rats_variant])+"; ctest --no-tests=error -j 16 -L 'rats' -L 'render|diff'"
+rats_cmd = "cd build/"+os.path.join(*variants[rats_variant])+"; ctest --no-tests=error -j 16 -L 'rats' -L 'render|diff' --output-on-failure"
 rats_testentry = { "command": rats_cmd,
-                    "requires": ["cmake"] + variants[rats_variant] }
+                         "requires": ["cmake"],
+                         "on_variants": {
+                             "type": "requires",
+                             "value": [".openmoonray_variant-%d" % rats_variant]
+                         },
+                         "run_on": "explicit"
+                     }
 testlist.append(("rats", rats_testentry))
+
+# Next, add retry rats tests entry
+rats_variant = 0
+retry_rats_cmd = "cd build/"+os.path.join(*variants[rats_variant])+"; ctest --no-tests=error -j 16 -L 'rats' -L 'render|diff' --rerun-failed --output-on-failure"
+retry_rats_testentry = { "command": retry_rats_cmd,
+                         "requires": ["cmake"],
+                         "on_variants": {
+                             "type": "requires",
+                             "value": [".openmoonray_variant-%d" % rats_variant]
+                         },
+                         "run_on": "explicit"
+                     }
+testlist.append(("retry_rats", retry_rats_testentry))
+
+# Next, add rats_debug tests entry
+rats_debug_variant = 1
+rats_debug_cmd = "cd build/"+os.path.join(*variants[rats_debug_variant])+"; ctest --no-tests=error -j 16 -L 'rats' -L 'render' --output-on-failure"
+rats_debug_testentry = { "command": rats_debug_cmd,
+                         "requires": ["cmake"],
+                         "on_variants": {
+                             "type": "requires",
+                             "value": [".openmoonray_variant-%d" % rats_debug_variant]
+                         },
+                         "run_on": "explicit"
+                     }
+testlist.append(("rats-debug", rats_debug_testentry))
+
+# Next, add retry_rats_debug tests entry
+rats_debug_variant = 1
+retry_rats_debug_cmd = "cd build/"+os.path.join(*variants[rats_debug_variant])+"; ctest --no-tests=error -j 16 -L 'rats' -L 'render' --rerun-failed --output-on-failure"
+retry_rats_debug_testentry = { "command": retry_rats_debug_cmd,
+                               "requires": ["cmake"],
+                               "on_variants": {
+                                   "type": "requires",
+                                   "value": [".openmoonray_variant-%d" % rats_debug_variant]
+                               },
+                               "run_on": "explicit"
+                           }
+testlist.append(("retry_rats-debug", retry_rats_debug_testentry))
 
 # Build final dictionary for rez-test command
 tests = dict(testlist)
